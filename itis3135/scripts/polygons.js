adee62e350ab08b91b5481ece95f4c51f3aa2f04
nicoleechevarria <Nechevar@uncc.edu>
2022-12-15 11:40:56 -0500

    please god i cant look at these shapes anymore

--- itis3135/scripts/polygons.js
+++ itis3135/scripts/polygons.js
@@ -1,6 +1,6 @@
 const polygons = () => 
 {
-    let number = prompt("The Noble Elk requests a number from 0 to 10...")
+    let number = prompt("The Noble Elk requests a number from 1 to 10...")
         if (validateEntry(number) === true) 
         {
             entered_number = Math.round(Math.abs(number))
@@ -13,36 +13,50 @@ const getShape = (entered_number) =>
 {
     switch (entered_number) 
     {
+        case 0:
+            polygon = "that's not a polygon"
+            break;
         case 1:
-            polygon = "henagon";
+            polygon = "that's a henagon"
             break;
         case 2:
-            polygon = "digon";
+            polygon = "that's a digon"
             break;
         case 3:
-            polygon = "trigon";
+            polygon = "that's a trigon"
             break;
         case 4:
-            polygon = "tetragon"
+            polygon = "that's a tetragon"
             break;
         case 5:
-            polygon = "pentagon"
+            polygon = "that's a pentagon"
             break;
         case 6:
-            polygon = "hexagon"
+            polygon = "that's a hexagon"
             break;
         case 7:
-            polygon = "heptagon"
+            polygon = "that's a heptagon"
             break;
         case 8:
-            polygon = "octagon"
+            polygon = "that's a octagon"
             break;
         case 9:
-            polygon = "enneagon"
+            polygon = "that's a enneagon"
             break;
         case 10:
-            polygon = "decagon"
+            polygon = "that's a decagon"
             break;
     }
     return polygon;
+}
+const validateEntry = (number) => {
+    if (isNaN(number)) {
+        alert("Thats not a number silly")
+        return false
+    } else if (number > 10 || number < -10) {
+        alert("Thats not within 1 to 10")
+        return false
+    } else {
+        return true
+    }
 }
