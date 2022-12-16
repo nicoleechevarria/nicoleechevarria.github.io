aff90fbd25e2522c157dfae8560f15162b6e4844
nicoleechevarria <Nechevar@uncc.edu>
2022-12-16 13:10:03 -0500

    activity 14?

--- itis3135/Activity13/EchevarriaActivity13.js
+++ itis3135/Activity13/EchevarriaActivity13.js
@@ -1,28 +1,3 @@
-/*
-$(document).ready(function() {
-    $.ajax({
-        type: "get",
-        url: "team.xml",
-        beforeSend: function() {
-            $("#team").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(xhr, status, error) {
-            alert("Error: " + xhr.status + " - " + error);
-        },
-        dataType: "xml",
-        success: function(data) {
-            $("#team").html("");
-            $(data).find("management").children().each(function() {
-                var xmlDoc = $(this);
-                $("#team").append
-                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
-                          xmlDoc.find("title").text() + "<br>" +
-                          xmlDoc.find("bio").text() + "<br>");
-            });
-        }
-    });
-}); */
 $(document).ready(function () {
     $.ajax({
         type: "get",
@@ -47,3 +22,16 @@ $(document).ready(function () {
                 });
             });
         }
+        // dataType: "xml",
+        // success: function(data) {
+        //     $("#team").html("");
+        //     $(data).find("management").children().each(function() {
+        //         var xmlDoc = $(this);
+        //         $("#team").append
+        //         ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
+        //                   xmlDoc.find("title").text() + "<br>" +
+        //                   xmlDoc.find("bio").text() + "<br>");
+        //     });
+        // }
+    });
+});
--- itis3135/Activity14/EchevarriaActivity14.js
+++ itis3135/Activity14/EchevarriaActivity14.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+    $("#nav_list li").click(function () {
+        var title = $(this).children("a").attr("title");
+        $.get("json_files/" + title + ".json", function (data, status) {
+            data = data['speakers'][0];
+            $("main h1").html(data['title']);
+            $("main h2").html(data['month']);
+            $("main h3").html(data['speaker']);
+            $("main img").attr("src", data.image);
+            $("main p").html(data.text);
+        });
+    });
 }); // end ready
